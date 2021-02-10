9e16910052e2d2112484dc6aede7bd2b0cc37df0
aso wang <aklatsan@krutt.org>
2021-02-10 15:51:01 +0800

    new exercise collatz

---
+++ collatz.c
@@ -0,0 +1,29 @@
+/* Performs Collatz Conjecture */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+        int n, steps = 0;
+        if (argc != 2) {
+                printf("Usage: ./collatz INTEGER\n");
+                return -1;
+        }
+
+        int n = atoi(argv[1]);
+        if (n <= 1) {
+                printf("Error: should be n > 1\n");
+                return -1;
+        }
+
+        while (n != 1) {
+                if (n % 2 == 0) {
+                        n /= 2;
+                } else {
+                        n = n * 3 + 1;
+                }
+                steps++;
+        }
+
+        printf("As per Collatz Conjecture, %d steps are needed to get to n = 1.\n", steps);
+        return 0;
+}
