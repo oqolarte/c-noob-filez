3e7dfd12eadd84ad62b93c740ae812ef3bb62a72
aso wang <aklatsan@krutt.org>
2021-04-28 19:40:56 +0800

    minor edit

--- 2dnest.c
+++ 2dnest.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-
     int nums[3][2] = {
                     {1, 2},
                     {3, 4},
                     {5, 6}
                     };
-
     int i, j;
     for(i = 0; i < 3; i++){
         for(j = 0; j < 2; j++){
